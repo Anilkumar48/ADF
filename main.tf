5637a2ba120dd1669660bad2a5e10794d99ca330
Anilkumar48 <50729716+Anilkumar48@users.noreply.github.com>
2021-07-27 17:15:59 +0530

    Update main.tf
    dfhdafh

--- main.tf
+++ main.tf
@@ -84,4 +84,4 @@ resource "azurerm_data_factory" "example" {
   name                = "${var.windows_virtual_machine}-ADF"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-}
+}xcvbx

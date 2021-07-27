c3a1b38071ef211a5d520999ce3fd0b75dd61997
Anilkumar48 <50729716+Anilkumar48@users.noreply.github.com>
2021-07-27 17:15:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "example1" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.1.0.0/26"]
 }
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "example" {  
   name                 = "${var.windows_virtual_machine}-sub-1"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
